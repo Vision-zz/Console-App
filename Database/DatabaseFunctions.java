0fb9b3db97dce043da3b22b3bfd05cf0262b2f98
Vision <74459856+Vision-zz@users.noreply.github.com>
2022-10-10 13:16:08 +0530

    Access modifier in DatabaseFunction interface

--- Database/DatabaseFunctions.java
+++ Database/DatabaseFunctions.java
@@ -2,12 +2,12 @@ package Database;
 
 public interface DatabaseFunctions<T> {
 
-    T get(String ID);
+    public T get(String ID);
 
-    void add(T t) throws Exception;
+    public void add(T t) throws Exception;
 
-    void remove(T t);
+    public void remove(T t);
 
-    void udpate(T t) throws Exception;
+    public void udpate(T t) throws Exception;
 
 }
