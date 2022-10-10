6170cff1c6844876017e93f2a5497a855685fb46
Vision <74459856+Vision-zz@users.noreply.github.com>
2022-10-10 12:14:06 +0530

    Base commit Session.java

--- Database/Session.java
+++ Database/Session.java
@@ -1,5 +1,22 @@
 package Database;
 
+import Users.Employee;
+
 public class Session {
-    
+    private static Employee loggedInAs = null;
+
+    private Session() {
+    }
+
+    public static Employee getLoggedInUser() {
+        return loggedInAs;
+    }
+
+    public static void login(Employee emp) {
+        loggedInAs = emp;
+    }
+
+    public static void logOut() {
+        loggedInAs = null;
+    }
 }
