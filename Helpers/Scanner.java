3ca618a79d2e4705bf89e60f1eb5013c14d76854
Vision <74459856+Vision-zz@users.noreply.github.com>
2022-10-10 15:52:16 +0530

    Modify Scanner formatting and refactor duplicate code

--- Helpers/Scanner.java
+++ Helpers/Scanner.java
@@ -3,64 +3,71 @@ package Helpers;
 public final class Scanner {
 
     private static final java.io.Console CONSOLE = System.console();
-    private static final String PROMPT_END = "\n" + StringFormat.formatWhite("");
+    private static final String PROMPT_END = StringFormat.formatWhite("");
 
     private Scanner() {
     }
 
     public static final String getString() {
-        return getString(null);
+        return getString("");
     }
 
     public static final String getString(String prompt) {
-        return prompt == null ? CONSOLE.readLine() : CONSOLE.readLine(prompt + PROMPT_END);
+        return CONSOLE.readLine(formatPrompt(prompt));
     }
 
     public static final int getInt() throws Exception {
-        return getInt(null);
+        return getInt("");
     }
 
     public static final int getInt(String prompt) throws Exception {
-        String input = prompt == null ? CONSOLE.readLine() : CONSOLE.readLine(prompt + PROMPT_END);
+        String input = CONSOLE.readLine(formatPrompt(prompt));
         return Integer.parseInt(input);
     }
 
     public static final double getDouble() throws Exception {
-        return getDouble(null);
+        return getDouble("");
     }
 
     public static final double getDouble(String prompt) throws Exception {
-        String input = prompt == null ? CONSOLE.readLine() : CONSOLE.readLine(prompt + PROMPT_END);
+        String input = CONSOLE.readLine(formatPrompt(prompt));
         return Double.parseDouble(input);
     }
 
     public static final float getFloat() throws Exception {
-        return getFloat(null);
+        return getFloat("");
     }
 
     public static final float getFloat(String prompt) throws Exception {
-        String input = prompt == null ? CONSOLE.readLine() : CONSOLE.readLine(prompt + PROMPT_END);
+        String input = CONSOLE.readLine(formatPrompt(prompt));
         return Float.parseFloat(input);
     }
 
     public static final long getLong() throws Exception {
-        return getLong(null);
+        return getLong("");
     }
 
     public static final long getLong(String prompt) throws Exception {
-        String input = prompt == null ? CONSOLE.readLine() : CONSOLE.readLine(prompt + PROMPT_END);
+        String input = CONSOLE.readLine(formatPrompt(prompt));
         return Long.parseLong(input);
     }
 
     public static final char getChar() throws Exception {
-        return getChar(null);
+        return getChar("");
     }
 
     public static final char getChar(String prompt) throws Exception {
-        String input = prompt == null ? CONSOLE.readLine() : CONSOLE.readLine(prompt + PROMPT_END);
+        String input = CONSOLE.readLine(formatPrompt(prompt));
         if (input.length() > 1)
             throw new Exception("Character length is out of bounds");
         return input.toCharArray()[0];
+    }
+
+    private static final String formatPrompt(String rawPrompt) {
+        if (!rawPrompt.endsWith("\n") && !rawPrompt.equals(""))
+            rawPrompt += "\n";
+
+        return rawPrompt + PROMPT_END;
     }
 
 }
