45c9d4b45620c01a0be6d5456d40a3582d5b8165
Vision <74459856+Vision-zz@users.noreply.github.com>
2022-10-10 12:33:46 +0530

    Base commit IssuesDatabase

--- Database/IssuesDatabase.java
+++ Database/IssuesDatabase.java
@@ -1,5 +1,56 @@
 package Database;
 
-public class ReportDatabase {
-     
+import java.util.HashMap;
+
+import Helpers.Logger;
+import Modules.Issue;
+
+public class IssuesDatabase implements DatabaseFunctions<Issue> {
+     private final HashMap<String, Issue> issues;
+
+     private static IssuesDatabase instance = null;
+
+     public static IssuesDatabase getInstance() {
+          if (instance == null)
+               instance = new IssuesDatabase();
+          return instance;
+     }
+
+     private IssuesDatabase() {
+          this.issues = new HashMap<String, Issue>();
+     }
+
+     @Override
+     public Issue get(String ID) {
+          return this.issues.get(ID);
+     }
+
+     @Override
+     public void add(Issue issue) throws Exception {
+          if (this.issues.containsKey(issue.issueID)) {
+               throw new Exception("Issue with ID: " + issue.issueID
+                         + " already exists. If you are trying to update a issue, try using IssueDatabase.update() method");
+          }
+          this.issues.put(issue.issueID, issue);
+     }
+
+     @Override
+     public void remove(Issue issue) {
+          if (!this.issues.containsKey(issue.issueID)) {
+               Logger.logInfo("Issue with ID: " + issue.issueID + " does not exist. Operation cancelled");
+               return;
+          }
+          this.issues.remove(issue.issueID);
+
+     };
+
+     @Override
+     public void udpate(Issue issue) throws Exception {
+          if (this.issues.containsKey(issue.issueID)) {
+               throw new Exception("Issue with ID: " + issue.issueID
+                         + " does not exist. If you are trying to add a issue, try using IssueDatabase.add() method");
+          }
+          this.issues.put(issue.issueID, issue);
+     }
+
 }
