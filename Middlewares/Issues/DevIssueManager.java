b01587c09f6df72f353e0e6193913fae4f3c5b5b
Vision <74459856+Vision-zz@users.noreply.github.com>
2022-10-11 16:15:17 +0530

    Missed to commit this along with the developer.java commit. Dumb me

--- Middlewares/Issues/DevIssueManager.java
+++ Middlewares/Issues/DevIssueManager.java
@@ -1,8 +1,9 @@
 package Middlewares.Issues;
 
 import Modules.Issue;
+import Users.Developer;
 
 public interface DevIssueManager {
 	public void newIssueRequest(Issue issue);
-	public Issue[] requestCreatedIssues();
+	public Issue[] requestCreatedIssues(Developer developer);
 }
