11a7fda860ee71d50a1e1f7d055e367ff48d332b
Vision <74459856+Vision-zz@users.noreply.github.com>
2022-10-11 15:13:45 +0530

    Added Developer methods

--- Users/Developer.java
+++ Users/Developer.java
@@ -1,9 +1,36 @@
 package Users;
 
+import Middlewares.DevIssueManager;
+import Modules.Issue;
+import Modules.IssueCategory;
+
 public final class Developer extends Employee {
 
     public Developer(String username, String password, String employeeName) {
         super(username, password, employeeName);
+    }
+
+    public void createIssue(IssueCategory category, String description, DevIssueManager manager) {
+        Issue issue = new Issue(category, description);
+        manager.newIssueRequest(issue);
+    }
+
+    public Issue getIssueByID(String ID, DevIssueManager manager) {
+        Issue[] issues = manager.requestCreatedIssues();
+        Issue issue = null;
+
+        for (Issue i : issues) {
+            if (i.issueID.equals(ID)) {
+                issue = i;
+                break;
+            }
+        }
+
+        return issue;
+    }
+
+    public Issue[] getAllCreatedIssues(DevIssueManager manager) {
+        return manager.requestCreatedIssues();
     }
 
 }
