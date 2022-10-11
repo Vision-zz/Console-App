882c37159fd8cb910c5036ec41da54b7e0bcb6fa
Vision <74459856+Vision-zz@users.noreply.github.com>
2022-10-11 15:14:40 +0530

    Refactor

--- Middlewares/IssueUtil.java
+++ Middlewares/IssueUtil.java
@@ -3,14 +3,14 @@ package Middlewares;
 import Database.DBIssue;
 import Modules.Issue;
 
-public class IssueUtil {
+public final class IssueUtil {
 
-    public final DBIssue cloneToDBIssue(Issue issue) {
+    public static DBIssue cloneToDBIssue(Issue issue) {
         return new DBIssue(issue.issueID, issue.getCategory(), issue.getDescription(), issue.getAssignedEngineer(),
                 issue.getCreatedBy(), issue.getStatus(), issue.getCreatedAt(), issue.getResolvedAt());
     }
 
-    public final Issue cloneToIssue(DBIssue issue) {
+    public static Issue cloneToIssue(DBIssue issue) {
         return new Issue(issue.issueID, issue.getCategory(), issue.getDescription(), issue.getAssignedTo(),
                 issue.getCreatedBy(), issue.getStatus(), issue.getCreatedAt(), issue.getResolvedAt());
     }
